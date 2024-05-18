22b8efd9cb8327b884d8ffa1fd15bef9927de5a1
Leonid Pipchenkov <t70a80@gmail.com>
2024-05-18 15:52:36 +0300

    Adding a 2D array example

---
+++ src/main/java/net/happiness/The2DArrays.java
@@ -0,0 +1,28 @@
+package net.happiness;
+
+import java.util.Arrays;
+
+public class The2DArrays {
+
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        board[0][0] = 'O';
+        board[1][0] = 'O';
+        board[2][0] = 'O';
+
+        char[][] boardTwo = new char[][] {
+                new char[] {'O', '-', '-'},
+                new char[] {'O', '-', '-'},
+                new char[] {'O', '-', '-'}
+        };
+
+        System.out.println(Arrays.deepToString(board));
+        System.out.println(Arrays.deepToString(boardTwo));
+    }
+
+}
