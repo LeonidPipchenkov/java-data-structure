eb2ac63d14fa30952d56c789e58c0df7bf8a4fd6
Leonid Pipchenkov <t70a80@gmail.com>
2024-06-28 11:09:21 +0300

    Adding a hashCode example

--- src/main/java/net/happiness/TheMaps.java
+++ src/main/java/net/happiness/TheMaps.java
@@ -2,10 +2,16 @@ package net.happiness;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class TheMaps {
 
     public static void main(String[] args) {
+        hashMap();
+        hashFunction();
+    }
+
+    private static void hashMap() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Alex"));
         map.put(2, new Person("Alexa"));
@@ -27,6 +33,45 @@ public class TheMaps {
         map.forEach((key, person) -> System.out.println(key + " - " + person));
     }
 
-    record Person(String name) {}
+    private static void hashFunction() {
+        Map<Person, Diamond> map = new HashMap<>();
+        Person person = new Person("Jamila");
+        map.put(person, new Diamond("African Diamond"));
+        System.out.println(person.hashCode());
+        System.out.println(person.hashCode());
+        System.out.println(map.get(person));
+    }
+
+    static class Person {
+
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+    }
+
+    record Diamond(String name) {}
 
 }
